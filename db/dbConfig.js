474ff7726e97f559992de5bd5884f2efe965aca9
Max Mezalon <jmezalon@gmail.com>
2022-04-28 17:53:44 -0400

    working on deploying database

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,12 +1,17 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const cn = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-};
+const cn =
+  process.env.NODE_ENV !== "production"
+    ? {
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT,
+        database: process.env.PG_DATABASE,
+        user: process.env.PG_USER,
+      }
+    : {
+        connetionString: process.env.DATABASE_URL,
+      };
 
 const db = pgp(cn);
 
