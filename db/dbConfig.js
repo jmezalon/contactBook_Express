26af27963969078db3c92a88b71be8286e5fc54a
Max Mezalon <jmezalon@gmail.com>
2022-04-28 18:34:19 -0400

    trying to deploy database

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -7,6 +7,7 @@ const cn =
         host: process.env.PG_HOST,
         port: process.env.PG_PORT,
         database: process.env.PG_DATABASE,
+        password: process.env.PG_PASSWORD,
         user: process.env.PG_USER,
       }
     : {
